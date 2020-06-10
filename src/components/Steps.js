5819f2463a9272454fb6bd07751aeeed07f90eb0
peter zaccha <p.pator@outlook.com>
2020-06-10 17:40:26 +0200

    add nodes

--- src/components/Steps.js
+++ src/components/Steps.js
@@ -13,8 +13,8 @@ const Steps = () => {
           <Modal.Description>
             <Image wrapped fluid src="/steps/1.jpeg" />
             <Header>
-              If you want to add node type the name of the node (example f) and
-              click on “add node” button.
+              If you want to add node <br /> type the name of the node (example
+              f) and click on “add node” button.
             </Header>
             <Message
               icon="hand point right"
@@ -26,12 +26,12 @@ const Steps = () => {
             ></Message>
             <Image wrapped fluid src="/steps/2.jpeg" />
             <Header>
-              If you want to delete a node type the name of the node (example C
-              ) and click on “delete” button.
+              If you want to delete a node <br /> type the name of the node
+              (example C ) and click on “delete” button.
             </Header>
             <Image wrapped fluid src="/steps/3.jpeg" />
             <Header>
-              If you want to add an edge type the name of the source node
+              If you want to add an edge <br /> type the name of the source node
               (example f ),the destination node (example E ) and the
               weight/Capacity of the edge (example 3 ) and click on “add edge”
               button.
